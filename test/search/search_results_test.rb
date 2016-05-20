923125be5fec16cca1ca99dc2c603615841fe23d
Joe Sak <joe@joesak.com>
2016-05-20 00:02:27 -0400

    Search 'SearchableList' by zipcode

--- test/search/search_results_test.rb
+++ test/search/search_results_test.rb
@@ -3,6 +3,16 @@ require "./test/search/searchable_list"
 
 class SearchResultsTest < Minitest::Test
   def test_searchable_list_finds_zero_results
-    assert_empty(SearchableList.query(some_attribute: 'nothing'))
+    SearchableList.stub(:search_by_zipcode, []) do
+      assert_empty(SearchableList.query(zipcode: 60610))
+    end
+  end
+
+  def test_searchableList_finds_one_result
+    search_result = Minitest::Mock.new
+
+    SearchableList.stub(:search_by_zipcode, [search_result]) do
+      assert_equal([search_result], SearchableList.query(zipcode: 60610))
+    end
   end
 end
--- test/search/searchable_list.rb
+++ test/search/searchable_list.rb
@@ -1,5 +1,10 @@
 class SearchableList
   def self.query(options)
-    []
+    options.flat_map do |attribute, value|
+      send("search_by_#{attribute}", value)
+    end
+  end
+
+  def self.search_by_zipcode(zipcode)
   end
 end
