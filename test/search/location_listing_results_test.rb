9f463049ee7ea381c4476b56344e9e7063b663c7
Joe Sak <joe@joesak.com>
2016-05-19 22:17:34 -0400

    Search by zipcode and get no results

--- test/search/location_listing_results_test.rb
+++ test/search/location_listing_results_test.rb
@@ -1,14 +1,26 @@
 require "test_helper"
 
 class LocationListingResultsTest < Minitest::Test
-  def test_find_zero_results
-    display = Display.new
+  def test_find_zero_results_by_zipcode
+    display = SearchResultsDisplay.new
+    search = Search.new(display)
+
+    search.by_zipcode(60610)
+
     assert_equal("Sorry! We couldn't find anything for you :(", display.results_text)
   end
 
-  class Display
+  class SearchResultsDisplay
     def results_text
       "Sorry! We couldn't find anything for you :("
+    end
+  end
+
+  class Search
+    def initialize(display)
+    end
+
+    def by_zipcode(zipcode)
     end
   end
 end
